7d05a5775d0d2ef806641699d1c2c93db1161179
raddva <111165508+raddva@users.noreply.github.com>
2025-11-30 23:04:15 +0700

    add: Breadcrumbs

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,13 +1,32 @@
-// import AppSidebar from '@/components/common/app-sidebar';
-
-import { ReactNode } from "react";
+import AppSidebar from '@/components/common/app-sidebar';
+import { Separator } from '@/components/ui/separator';
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from '@/components/ui/sidebar';
+import { ReactNode } from 'react';
+import DashboardBreadcrumb from './admin/_components/dashboard-breadcrumb';
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
     return (
-        <div>
-            <main className="flex flex-1 flex-col items-start gap-4 p-4 pt-0">
-                {children}
-            </main>
-        </div>
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset className="overflow-x-hidden">
+                <header className="flex justify-between h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                    <div className="flex items-center gap-2 px-4">
+                        <SidebarTrigger className="cursor-pointer" />
+                        <Separator
+                            orientation="vertical"
+                            className="mr-2 data-[orientation=vertical]:h-4"
+                        />
+                        <DashboardBreadcrumb />
+                    </div>
+                </header>
+                <main className="flex flex-1 flex-col items-start gap-4 p-4 pt-0">
+                    {children}
+                </main>
+            </SidebarInset>
+        </SidebarProvider>
     );
 }
