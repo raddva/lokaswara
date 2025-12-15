9a0bf2754e6a9f170cb6e7566efc07a3a7f23e24
Reksaditya <reksa@ReksaAditya.localdomain>
2025-12-15 08:40:58 +0700

    fix: remove input box

--- src/app/(information)/makanan/_components/makanan.tsx
+++ src/app/(information)/makanan/_components/makanan.tsx
@@ -72,7 +72,6 @@ export default function MakananSundaPage() {
       <div className="bg-linear-to-b from-black/70 to-black to-95% inset-0 absolute z-10 w-full h-full"></div>
       <div className="relative z-20 flex flex-col gap-10">
         <h1 className="font-bold text-5xl">Jelajahi Makanan Khas Sunda</h1>
-        <Input className="bg-white" />
         <div className="grid grid-cols-4 gap-3">
           {foodItems.map((item, index) => {
             return (
