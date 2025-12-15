081198d17a63b00413ecfc65db743d7885a37954
Reksaditya <reksa@ReksaAditya.localdomain>
2025-12-15 20:53:01 +0700

    feat: footer

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -2,6 +2,7 @@ import Hero from './_components/hero';
 import Penjelasan from './_components/penjelasan';
 import Category from './_components/category';
 import VisiMisi from './_components/visimisi';
+import Footer from '@/components/common/app-footer';
 
 export const metadata = {
     title: 'LokaSwara | Lestarikan Budaya Melalui Sistem Digital',
@@ -14,6 +15,7 @@ export default function HomePage() {
             <Penjelasan />
             <Category />
             <VisiMisi />
+            <Footer />
         </main>
     );
 }
--- src/app/(information)/layout.tsx
+++ src/app/(information)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/common/app-footer"
 
 
 export default function RootLayout({
@@ -8,6 +9,7 @@ export default function RootLayout({
   return (
     <main>
       {children}
+      <Footer />
     </main>
   )
 }
