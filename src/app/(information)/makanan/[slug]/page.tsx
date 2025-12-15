f7d20c427d7a93e3d24e8da7544ab779c987fd63
Reksaditya <reksa@ReksaAditya.localdomain>
2025-12-16 01:40:28 +0700

    feat: makanan detail metadata

--- src/app/(information)/makanan/[slug]/page.tsx
+++ src/app/(information)/makanan/[slug]/page.tsx
@@ -1,6 +1,9 @@
-"use client"
 import Navbar from "@/components/common/app-navbar"
 import MakananDetail from "./_components/slug"
+
+export const metadata = {
+    title: 'LokaSwara | Detail Makanan',
+};
 
 export default function MakananDetailPage() {
   return (
